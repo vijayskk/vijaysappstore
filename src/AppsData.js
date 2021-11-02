4fd8617bc03d3b4b1aed6094c380e08bdb797b99
Vijay Satheesh <83910193+vijayskk@users.noreply.github.com>
2021-11-02 16:37:21 +0530

    Bmi Calculator added

--- src/AppsData.js
+++ src/AppsData.js
@@ -1,7 +1,14 @@
 
 const appData = [
-    
-
+        {
+        name:"Bmi Calculator",
+        category:"health",
+        madeby:"Vijay Satheesh",
+        description:"A good app Made by Vijay.\n You can measure your bmi with weight and height...",
+        iconfile:"bmiicon.png",
+        appfile:"Bmi.Calculator.v3.apk",
+        sizeinmb:18.9
+        },
 ]
 
 
