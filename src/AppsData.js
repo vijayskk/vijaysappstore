1eef38ddeb71a0b6807ba72bec5a3096f99bee5e
Vijay Satheesh <83910193+vijayskk@users.noreply.github.com>
2021-11-05 12:50:39 +0530

    Todo app appdata updated

--- src/AppsData.js
+++ src/AppsData.js
@@ -9,6 +9,15 @@ const appData = [
         appfile:"Bmi.Calculator.v3.apk",
         sizeinmb:18.9
         },
+        {
+        name:"Todo App",
+        category:"education",
+        madeby:"Vijay Satheesh",
+        description:"A simple todo app made by vijay",
+        iconfile:"todologo.jpg",
+        appfile:"Todo.app.V1.apk",
+        sizeinmb:17.9
+        },
 ]
 
 
