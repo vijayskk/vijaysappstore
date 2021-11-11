b39c7b543f602648bc66adeaf4d51d0d5292ce64
vijayskk <vijayskk007@gmail.com>
2021-11-11 15:13:03 +0530

    Flybooks added

--- src/AppsData.js
+++ src/AppsData.js
@@ -1,32 +1,41 @@
 
 const appData = [
-        {
-        name:"Bmi Calculator",
-        category:"health",
-        madeby:"Vijay Satheesh",
-        description:"A good app Made by Vijay.\n You can measure your bmi with weight and height...",
-        iconfile:"bmiicon.png",
-        appfile:"Bmi.Calculator.v3.apk",
-        sizeinmb:18.9
-        },
-        {
-        name:"Todo App",
-        category:"education",
-        madeby:"Vijay Satheesh",
-        description:"A simple todo app made by vijay",
-        iconfile:"todologo.jpg",
-        appfile:"Todo.app.V2.apk",
-        sizeinmb:17.9
-        },
-        {
-        name:"World Time",
-        category:"education",
-        madeby:"Vijay Satheesh",
-        description:"An app to view time in different zones",
-        iconfile:"worldtimelogo.png",
-        appfile:"World.Time.V2.apk",
-        sizeinmb:20.3
-        },
+    {
+        name: "Bmi Calculator",
+        category: "health",
+        madeby: "Vijay Satheesh",
+        description: "A good app Made by Vijay.\n You can measure your bmi with weight and height...",
+        iconfile: "bmiicon.png",
+        appfile: "Bmi.Calculator.v3.apk",
+        sizeinmb: 18.9
+    },
+    {
+        name: "Todo App",
+        category: "education",
+        madeby: "Vijay Satheesh",
+        description: "A simple todo app made by vijay",
+        iconfile: "todologo.jpg",
+        appfile: "Todo.app.V2.apk",
+        sizeinmb: 17.9
+    },
+    {
+        name: "World Time",
+        category: "education",
+        madeby: "Vijay Satheesh",
+        description: "An app to view time in different zones",
+        iconfile: "worldtimelogo.png",
+        appfile: "World.Time.V2.apk",
+        sizeinmb: 20.3
+    },
+    {
+        name: "Flybooks App",
+        category: "fun",
+        madeby: "Vijay Satheesh",
+        description: "An app for searching and finding books.\nReading is always good :-)",
+        iconfile: "flybookslogo.png",
+        appfile: "Flybooks.V1.apk",
+        sizeinmb: 17.3
+    },
 ]
 
 
